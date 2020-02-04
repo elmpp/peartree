b1baba28306264485705b31daa7d5e44c1b7db16
matt penrice <matthew.penrice@gmail.com>
2020-02-04 16:17:13 +0100

    wip: 💪 READMEs are a lot betetr

--- packages/typescript-aliases/typings/graphql.d.ts
+++
@@ -1,8 +0,0 @@
-// https://goo.gl/wUE7Ed
-declare module '*.graphql' {
-  const content: import('graphql').DocumentNode
-  // export = content;
-  export default content
-}
-
-type GRAPHQL_DOCUMENT = import('graphql').DocumentNode | string
